bde051749e6f95b13397c19c14155eb5eb8cfb15
KL-4h26 <pcode922@gmail.com>
2025-07-20 18:25:05 +0300

    Update bot.py (add /set_model)

--- bot.py
+++ bot.py
@@ -11,18 +11,36 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 genai.configure(api_key=GEMINI_TOKEN)
 
-model = genai.GenerativeModel(MODEL)
+chat_model = genai.GenerativeModel(model)
 
 # Создание чата
-chat = model.start_chat()
+chat = chat_model.start_chat()
 
 # Вводный промпт
 response = chat.send_message(START_PROMPTS["base"])
 
+@dp.message(Command("set_model"))
+async def set_model(message: Message):
+    global model
+
+    if str(message.from_user.id) in ADMINS:
+        get_model = re.search(r"^/set_model\s(.+)", message.text)
+
+        if get_model != None:
+            model = get_model.group(1)
+            await message.answer("✅ Успешно изменена модель")
+
+        else:
+            await message.answer("❗ Пожалуйста укажите модель (/set_model <имя модели>)")
+
+    else:
+        await message.answer("❗ У вас нет прав на выполнение команды")
+
+
 @dp.message(Command("reload"))
 async def reload_chat(message: Message):
     global chat
-    chat = model.start_chat()
+    chat = chat_model.start_chat()
     await message.answer("❗Создан новый чат")
     response = chat.send_message(START_PROMPTS["base"])
 
